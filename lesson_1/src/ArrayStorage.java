deda1da97ac4cd9da51a6b143b3ab3448e168cbb
Andrey Tokarev <aa.tokarev@it-solutions.su>
2023-04-18 18:10:08 +0300

    General commit

---
+++ lesson_1/src/ArrayStorage.java
@@ -0,0 +1,57 @@
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    Resume[] storage = new Resume[10000];
+    int size = 0;  // Количество резюме
+
+    void clear() {
+        for (int i = 0; i < size; i++)
+            storage[i] = null;
+
+        size = 0;
+    }
+
+    void save(Resume r) {
+        storage[size] = r;
+        size++;
+    }
+
+    Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid))
+                return storage[i];
+        }
+        return null;
+    }
+
+    void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+
+            if (storage[i].uuid.equals(uuid)) {
+
+                storage[i] = null;
+
+                // Shift elements after deletion
+                for (int j = i + 1; j < size; j++) {
+                    storage[j - 1] = storage[j];
+                }
+                size--;
+                break;
+            }
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        Resume[] allResume = new Resume[size];
+        System.arraycopy(storage, 0, allResume, 0, size);
+        return allResume;
+    }
+
+    int size() {
+        return size;
+    }
+}
