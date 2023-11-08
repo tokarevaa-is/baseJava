2624be7e8fb7192547d7c689628e3bccfad90252
Andrey Tokarev <aa.tokarev@it-solutions.su>
2023-11-08 10:34:39 +0300

    Solution for Lesson 13 (SQLStorage added).

---
+++ src/com/tokarevaa/webapp/storage/SQLStorage.java
@@ -0,0 +1,94 @@
+package com.tokarevaa.webapp.storage;
+
+import com.tokarevaa.webapp.exception.NotExistStorageException;
+import com.tokarevaa.webapp.model.Resume;
+import com.tokarevaa.webapp.sql.SQLHelper;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLStorage implements Storage {
+
+    private final SQLHelper sqlHelper;
+
+    public SQLStorage(String dbUrl, String dbUser, String dbPassword) {
+        sqlHelper = new SQLHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
+    }
+
+    @Override
+    public void clear() {
+        sqlHelper.execute("DELETE FROM resume");
+    }
+
+    @Override
+    public void save(Resume r) {
+
+        sqlHelper.execute("INSERT INTO resume (uuid, full_name) VALUES (?,?)", ps -> {
+            ps.setString(1, r.getUuid());
+            ps.setString(2, r.getFullName());
+            ps.execute();
+            return null;
+        });
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        return sqlHelper.execute("SELECT * FROM resume r WHERE r.uuid =?", ps -> {
+            ps.setString(1, uuid);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                throw new NotExistStorageException(uuid);
+            }
+            return new Resume(uuid, rs.getString("full_name"));
+        });
+    }
+
+    @Override
+    public void delete(String uuid) {
+        sqlHelper.execute("DELETE FROM resume r WHERE r.uuid =?", ps -> {
+            ps.setString(1, uuid);
+            if (ps.executeUpdate() == 0) {
+                throw new NotExistStorageException(uuid);
+            }
+            return null;
+        });
+    }
+
+    @Override
+    public void update(Resume resume) {
+        sqlHelper.execute("UPDATE resume r SET full_name = ? WHERE r.uuid = ?", ps -> {
+            ps.setString(1, resume.getFullName());
+            ps.setString(2, resume.getUuid());
+            if (ps.executeUpdate() == 0) {
+                throw new NotExistStorageException(resume.getUuid());
+            }
+            return null;
+        });
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        return sqlHelper.execute("SELECT * FROM resume ORDER BY full_name", ps -> {
+            ResultSet rs = ps.executeQuery();
+            List<Resume> resumes = new ArrayList<>();
+            while (rs.next()) {
+                resumes.add(new Resume(rs.getString("uuid"), rs.getString("full_name")));
+            }
+            return resumes;
+        });
+    }
+
+    @Override
+    public int size() {
+        return sqlHelper.execute("SELECT count(*) FROM resume", ps -> {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                return rs.getInt(1);
+            } else {
+                return 0;
+            }
+        });
+    }
+}
